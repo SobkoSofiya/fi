6e30183892ec7fdb2be6ffbd53c6e51b79807ba8
SobkoSofiya <sobkosofiya23@gmail.com>
2021-02-08 17:01:05 +0300

    Initial Commit

---
+++ Fix/FixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixApp.swift
+//  Fix
+//
+//  Created by Sofi on 08.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct FixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
